9add59fc096d4cc9ee20970d29f6012ad212f789
Juliana Stahelin <julianastahelin@gmail.com>
2023-03-15 20:39:32 -0300

    corrections in HTML, CSS and JS

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-const setaDireita = document.querySelector('.seta-direita');
+const nextArrow = document.querySelector('.seta-direita');
+const previousArrow = document.querySelector('.seta-esquerda');
 
-setaDireita.addEventListener('click', function() {
-    let slide1 = document.querySelector('.slide-1');
-    let slide2 = document.querySelector('.slide-2')
-    slide1.classList.add('hide');
-    slide2.classList.remove('hide'); 
+let slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+nextArrow.addEventListener('click', function() {
+    if (slides[slides.length - 1].classList.contains('show')) {
+        return;
+    } else {
+        currentSlide ++;
+        hideShowingSlide();
+        showSlide();
+        activateOrInactivateArrow();
+    }
 })
+
+previousArrow.addEventListener('click', function() {
+    if (slides[0].classList.contains('show')) {
+        return;
+    } else {
+        currentSlide --;
+        hideShowingSlide();
+        showSlide();
+        activateOrInactivateArrow();
+    }
+})
+
+function showSlide() {
+    slides[currentSlide].classList.add('show');
+}
+
+function hideShowingSlide() {
+    const slideWithShow = document.querySelector('.show');
+    slideWithShow.classList.remove('show');
+}
+
+function activateOrInactivateArrow() {
+    if (currentSlide === 0) {
+        previousArrow.classList.add('inactive');
+    } else {
+        previousArrow.classList.remove('inactive');
+    }
+
+    if (currentSlide === (slides.length - 1)) {
+        nextArrow.classList.add('inactive');
+    } else {
+        nextArrow.classList.remove('inactive');
+    }
+}
